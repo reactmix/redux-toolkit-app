4e069f23ac536bce18b69464f832267b303b15ae
Andy <andy@Andys-MacBook-Pro.local>
2021-01-26 23:27:33 -0500

    use the store in a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { setPosts, addPost, removePost, selectPosts } from './store/posts'
+import { useEffect } from 'react';
+
+function fetchServerData(){
+  return new Promise((resolve) => {
+    resolve([
+      { id: 1, content: 'First Posts' },
+      { id: 2, content: 'Second Posts' },
+      { id: 3, content: 'Third Posts' },
+    ])
+  })
+}
+
+function getNewPost(){
+  return {
+    id: new Date().getTime(),
+    content: new Date().toTimeString()
+  }
+}
 
 function App() {
+  const dispatch = useDispatch()
+  
+  const posts = useSelector(selectPosts)
+
+  useEffect(() => {
+    fetchServerData().then((data) => {
+      dispatch(setPosts({ posts: data }))
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {posts.map(x => (
+          <li key={x.id}>
+            <span>{ x.content }</span>
+            <button onClick={evt => 
+              dispatch(removePost({ postId: x.id }))
+            }>remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={evt => 
+        dispatch(addPost({ post: getNewPost() }))
+      }>Add Post</button>
     </div>
   );
 }
